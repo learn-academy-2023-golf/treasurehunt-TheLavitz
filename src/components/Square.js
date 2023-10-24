af1fb6e34b611e39bfd842b73c562f11efc288ca
TheLavitz <lavitz@gmail.com>
2023-10-24 14:17:29 -0700

    adds handleClick and handleGamePlay functions to pass index when clicked

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,15 @@
 import React from "react"
 
 const Square = (props) => {
+    const handleClick = () => {
+      // alert(props.index)
+      props.handleGamePlay(props.index)
+    }
   return (
     <>
-      <div className="square">{props.value}</div>
+      <div className="square" onClick={handleClick}>
+        {props.value}
+      </div>
     </>
   )
 }
